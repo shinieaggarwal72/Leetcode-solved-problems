4c419eb2c3b9e88e339a4d56f4fe10009ce92330
Shinie Aggarwal <shinieaggarwal72@gmail.com>
2024-08-24 00:41:06 +0530

    Time: 8 ms (77.51%), Space: 14.4 MB (19.45%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -3,13 +3,32 @@
 class Solution {
     public:             
     vector<int> twoSum(vector<int>& nums, int target) {
-        for(int i=0; i<nums.size(); i++){
+        /*for(int i=0; i<nums.size(); i++){
             for(int j=i+1; j<nums.size(); j++){
                 if((nums[i]+nums[j])==target)
                 {
                    return {i, j};
                 }
             }
-        }return {};
+        }return {};*/
+
+        /*unordered_map<int, int>hash;
+        for(int i=0; i<nums.size(); i++)
+            hash[nums[i]]=i;
+        for(int i=0; i<nums.size(); i++){
+            int comp = target - nums[i];
+            if(hash.find(comp)!=hash.end() && hash[comp]!=i)
+                return {i,hash[comp]};
+        }
+        return {};*/
+
+        unordered_map<int,int>hash;
+        for(int i=0; i<nums.size(); i++){
+            int comp = target - nums[i];
+            if(hash.find(comp)!=hash.end())
+                return {hash[comp],i};
+            hash[nums[i]] = i;
+        }
+        return {};
     }
 };
